fd29fbe036057a0fd996238385e6c9d56bbee594
Ho Bing Xuan <hobingxuan97@gmail.com>
2021-07-03 17:10:22 +0800

    updates BE env var

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const config = {
   LOCAL_HTTP_SERVER_URL: 'http://localhost:8000',
-  REMOTE_HTTP_SERVER_URL: 'https://sg-bridge.herokuapp.com',
+  REMOTE_HTTP_SERVER_URL: 'https://sg-bridge-api.herokuapp.com',
 };
