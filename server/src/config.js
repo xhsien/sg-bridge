f680eae44c974e107e02b61d1fcb5154526634c2
Ho Bing Xuan <hobingxuan97@gmail.com>
2021-07-04 10:56:41 +0800

    Revert "Adds FE to allowed URLs"
    This reverts commit f9d06357f773b79f1798d1c3b84411e7bdd8d780.

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ALLOWED_FRONTEND_SERVER_URL: ['http://192.168.0.114:3000', 'http://192.168.0.238:3000', 'https://sg-bridge.herokuapp.com/'],
+  ALLOWED_FRONTEND_SERVER_URL: ['http://192.168.0.114:3000', 'http://192.168.0.238:3000'],
   API_PORT: 8000,
   REDIS_URL: "", // get this from developer 
 };
